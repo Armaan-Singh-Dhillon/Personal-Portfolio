4e2256fbbcaa54dbc49461920136bdc38d2e5e30
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com>
2023-03-01 00:12:09 +0530

    admin page corrected

--- pages/admin.js
+++ pages/admin.js
@@ -3,76 +3,86 @@ import { MyContext } from '@/Context/Context.js';
 import { useState } from "react"
 import { useRouter } from 'next/router'
 import axios from "axios"
+import Loader from '@/components/Loader';
 
 
 const Admin = () => {
-  const { setState ,state } = useContext(MyContext);
+  const { setState, state } = useContext(MyContext);
 
-  
+
   const router = useRouter()
- 
-  const [email,setEmail]=useState('')
-  const [password,setPassword] = useState('')
 
-  useEffect(()=>{
-    if(state.token){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (state.token) {
       router.push('/dashboard')
 
     }
   })
- 
-  const handleSubmit=async(e)=>{
-   
-    const {data} = await axios.post('/api/admin',{plainEmail:email , plainPassword : password})
-    const {token} = data
 
-    if(token){
+  const handleSubmit = async (e) => {
+
+    const { data } = await axios.post('/api/admin', { plainEmail: email, plainPassword: password })
+    const { token } = data
+
+    if (token) {
       localStorage.setItem('token', token)
       setState({ ...state, token: token })
-       
+
       router.push('/dashboard')
+      setLoading(false)
 
     }
 
 
-   
+
   }
-  
-  return (
-    <>
-      <div className='flex text-white justify-evenly items-center p-8 mt-36'>
-        <div className='bg-gradient-to-bl from-indigo-500 via-purple-500 to-pink-500 w-3/12 p-8 rounded-2xl'>
-          <div className='flex justify-evenly'>
-            <div className='text-2xl'>
+  if (loading) {
+    return <Loader></Loader>
+  }
+  else {
 
-             Login
-            </div>
-          </div>
-          <div className='text-xl mb-4'>
-            Email
-          </div>
-          <div >
-            <input type="text" placeholder='Email' className='w-full p-4 mb-6 text-black' value={email} onChange={(e)=>setEmail(e.target.value)} />
-          </div>
-          
 
-          <div className='text-xl mb-4'>
-            Password
-          </div>
 
-          <div>
-            <input type="password" placeholder='Password' className='w-full p-4 mb-6 text-black' value={password} onChange={(e) => setPassword(e.target.value)}  />
-          </div>
-          
-          <div>
+    return (
+      <>
+        <div className='flex text-white justify-evenly items-center p-8 mt-36'>
+          <div className='bg-gradient-to-bl from-indigo-500 via-purple-500 to-pink-500 w-3/12 p-8 rounded-2xl'>
             <div className='flex justify-evenly'>
-              <button className='bg-blue-500 p-4 rounded-xl text-lg ' onClick={(e)=>handleSubmit(e)}>Submit</button>
+              <div className='text-2xl'>
+
+                Login
+              </div>
+            </div>
+            <div className='text-xl mb-4'>
+              Email
+            </div>
+            <div >
+              <input type="text" placeholder='Email' className='w-full p-4 mb-6 text-black' value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+
+
+            <div className='text-xl mb-4'>
+              Password
+            </div>
+
+            <div>
+              <input type="password" placeholder='Password' className='w-full p-4 mb-6 text-black' value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+
+            <div>
+              <div className='flex justify-evenly'>
+                <button className='bg-blue-500 p-4 rounded-xl text-lg ' onClick={(e) => handleSubmit(e)}>Submit</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default Admin
