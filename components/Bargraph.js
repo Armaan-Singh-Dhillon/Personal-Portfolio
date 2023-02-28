72ef6013e1c3464bee705c8efcbff7fd7d3b51e3
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com>
2023-02-28 19:01:50 +0530

    more functionality added to the charts

--- components/Bargraph.js
+++ components/Bargraph.js
@@ -21,35 +21,8 @@ ChartJS.register(
     Legend
 );
 
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top' ,
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Bar Chart',
-        },
-    },
-};
 
-const labels = ['Javascrript', 'Html', 'CSS' ,'c/c++','React.js' ,'Node.js','Express', 'Next.js' ,'MongoDB','Java','React-Native' ];
-const barData=[9.5,7,8.5,4,9.8,6,7,9.6,8,6,7.5]
-
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Skills',
-            data: barData.map((no)=>no),
-            backgroundColor: 'rgba(177, 79, 196,0.5)',
-            
-        },
-    
-    ],
-};
 
 export function BarGraph(options,data) {
-    return <Bar options={options} data={data} className='bg-white'  />;
+    return <Bar options={options} data={data} className='bg-white '  />;
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { MyContext } from "@/Context/Context";
 import { useState } from 'react';
 import axios from 'axios';
+import { BarGraph } from '@/components/Bargraph';
 export default function index() {
   const { setState, state } = useContext(MyContext);
 
@@ -14,6 +15,7 @@ export default function index() {
     const { data } = await axios.get('http://localhost:3000/api/skills/bar-chart/getAll')
     setData(data.data)
   }
+
   useEffect(() => {
     fetchData()
     if (!state.token) {
@@ -33,40 +35,94 @@ export default function index() {
         'Authorization': `Bearer ${state.token}`
       }
     })
-
+    fetchData()
 
   }
   if (!data) {
     return <h1>Loading</h1>
   }
   else {
-
+    const bardata = []
+    const labels = []
+    data.map(({ name, points, color }) => {
+      bardata.push(points)
+      labels.push(name)
+    })
 
 
     return (
       <>
+        <div className=' w-10/12'>
+          {BarGraph({
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Skills',
+              },
+            },
+          }, {
+            labels,
+            datasets: [
+              {
+                label: 'Skills',
+                data: bardata,
+                backgroundColor: 'rgba(177, 79, 196,0.5)',
+
+              },
+
+            ],
+          })}
+        </div>
 
         <div className=''>
-          <div className=' w-4/12 p-6'>
-            <div className='text-xl flex justify-evenly '>
+          <div className='  p-6 flex justify-between'>
+            <div className='w-5/12'>
 
-              add skill in graph
+
+              <div className='text-xl  justify-evenly '>
+
+                add skill in graph
+              </div>
+
+              <div className='m-2'>
+                name
+              </div>
+              <div className='m-2'>
+                <input type="text" name="" id="" className='w-full p-2 text-black' onChange={(e) => setname(e.target.value)} />
+              </div>
+              <div className='m-2'>
+                points
+              </div>
+              <div className='m-2'>
+                <input type="text" name="" id="" className='w-full p-2 text-black' onChange={(e) => setpoints(e.target.value)} />
+              </div>
+              <div className='flex justify-evenly '>
+                <button className='bg-purple-600 p-2 m-2' onClick={submitHandler}>add</button>
+              </div>
             </div>
 
-            <div className='m-2'>
-              name
-            </div>
-            <div className='m-2'>
-              <input type="text" name="" id="" className='w-full p-2 text-black' onChange={(e) => setname(e.target.value)} />
-            </div>
-            <div className='m-2'>
-              points
-            </div>
-            <div className='m-2'>
-              <input type="text" name="" id="" className='w-full p-2 text-black' onChange={(e) => setpoints(e.target.value)} />
-            </div>
-            <div className='flex justify-evenly '>
-              <button className='bg-purple-600 p-2 m-2' onClick={submitHandler}>add</button>
+            <div className='w-5/12'>
+
+
+              <div className='text-xl flex justify-evenly '>
+
+                delete skill in graph
+              </div>
+
+              <div className='m-2'>
+                name
+              </div>
+              <div className='m-2'>
+                <input type="text" name="" id="" className='w-full p-2 text-black' onChange={(e) => setname(e.target.value)} />
+              </div>
+              
+              <div className='flex justify-evenly '>
+                <button className='bg-purple-600 p-2 m-2' onClick={submitHandler}>Delete</button>
+              </div>
             </div>
           </div>
 
